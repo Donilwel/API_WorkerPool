2bc51b411fdc2972903e381682a18136796bfba4
Данила <dyumaslov@edu.hse.ru>
2024-11-04 16:28:06 +0300

    init: testaddjobhandler, testaddworkerhandler

--- cmd/myapp/test/handlers_test.go
+++ cmd/myapp/test/handlers_test.go
@@ -3,8 +3,10 @@ package test
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 	"workerPool/internal/api"
+	"workerPool/internal/worker"
 )
 
 func TestPingHandler(t *testing.T) {
@@ -21,4 +23,96 @@ func TestPingHandler(t *testing.T) {
 	if body != "PONG" {
 		t.Errorf("got %s, want PONG", body)
 	}
+}
+
+func TestAddJobHandler(t *testing.T) {
+	pool := worker.NewPool()
+	pool.AddWorker(1)
+	handler := api.AddJobHandler(pool)
+
+	t.Run("TestWithoutParameters", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/api/add_job?job=", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
+	})
+
+	t.Run("TestWithParameters", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/api/add_job?job=SomethingJob", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		handler.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		body := rr.Body.String()
+		example := "Задача добавлена: SomethingJob, ей занимается воркер с ID: 1\n"
+		if body != example {
+			t.Errorf("handler returned unexpected body: got\n %v want\n %v", body, example)
+		}
+	})
+}
+
+func TestAddWorkerHandler(t *testing.T) {
+	pool := worker.NewPool()
+	mu := sync.Mutex{}
+
+	handler := api.AddWorkerHandler(pool, &mu)
+
+	t.Run("AddWorker", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/api/add_worker", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		example := "Воркер добавлен с ID: 1\n"
+		body := rr.Body.String()
+
+		if body != example {
+			t.Errorf("handler returned unexpected body: got\n %v want\n %v", body, example)
+		}
+
+		if len(pool.Workers) != 1 {
+			t.Errorf("handler returned wrong len of workers: got %d, want %d", len(pool.Workers), 1)
+		}
+	})
+
+	t.Run("AddSecondWorker", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/api/add_worker", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		handler.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		example := "Воркер добавлен с ID: 2\n"
+		body := rr.Body.String()
+
+		if body != example {
+			t.Errorf("handler returned unexpected body: got\n %v want\n %v", body, example)
+		}
+
+		if len(pool.Workers) != 2 {
+			t.Errorf("handler returned wrong len of workers: got %d, want %d", len(pool.Workers), 2)
+		}
+	})
 }
